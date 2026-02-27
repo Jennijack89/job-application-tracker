a33db1cf1e1a603e028550bed54738c0cdf00937
JenniferJackson <marshall_jennifer3@students.columbusstate.edu>
2026-02-27 12:12:59 -0500

    Modified app.py by created get_db_connection() definition and get init_db() to create our user table

--- app.py
+++ app.py
@@ -12,6 +12,35 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.secret_key = "dev-secret-change-this" #needed for flash + session
 
+#Assign database file to DB_NAME
+DB_NAME = "app.db"
+
+# Connects to my DB in SQlite and Returns....
+def get_db_connection():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db_connection()
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS user(
+        user_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password_hash TEXT NOT NULL
+        )""")
+
+    # future tables fo here too.....
+    # conn.execute("""CREATE TABLE IF NOT EXISTS...""")
+    
+    conn.commit()
+    conn.close()
+    
+    init_db()
+   
+
+## My Routes
 @app.route("/")
 def home():
     return render_template("register.html")
