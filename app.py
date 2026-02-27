8200fea00ebcc8dffa91ad83b0a09d2b31ca2339
JenniferJackson <marshall_jennifer3@students.columbusstate.edu>
2026-02-27 11:57:40 -0500

    IMPORTANT BASE PYTHON COMMIT - has all imports and pre-setup needed for applictation (Flask Items, SQlite, Security-Hashing Import)

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+# JobAppTrack
+#CPSC 4205 - IT CapStone Project
+#Spring Semester 2026
+# Jennifer, Honey, Tyree,Zion
 
+# Import Flask items, SQlite, and Hashing
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#Assign Flask to app
 app = Flask(__name__)
+app.secret_key = "dev-secret-change-this" #needed for flash + session
 
 @app.route("/")
 def home():
